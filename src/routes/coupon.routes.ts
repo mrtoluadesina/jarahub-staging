2f3fa6b87432f20daae74eb966cb62d873bf9864
Tyav <tyav2greenz@gmail.com>
2020-03-07 20:19:00 +0100

    add code to get coupon

--- src/routes/coupon.routes.ts
+++ src/routes/coupon.routes.ts
@@ -1,15 +1,31 @@
 import { Router, Request, Response } from 'express';
-import { celebrate as validate, errors } from 'celebrate';
+import { errors } from 'celebrate';
 //import discountValidation from '../validations/discount.validation';
-//import * as discountController from '../controllers/disocunt.controller';
+import * as couponController from '../controllers/coupon.controller';
 //import adminAuth from '../middlewares/adminAuth';
 
 const router = Router();
+router.get('/', async (req: Request, res: Response) => {
+  return res.json({ message: 'original' })
+})
 
 //Get a Single Discount
-router.get('/', async (req: Request, res: Response) => {
-
+router.get('/:code', async (req: Request, res: Response) => {
+  try {
+    const { message, statusCode, error, payload } = await couponController.GetCoupon(
+      req.params.code
+    )
+  
+    return res.status(statusCode).json({
+      message, payload, error, statusCode
+    }) 
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message, statusCode: error.status
+    }) 
+  }
 });
+
 
 router.use(errors());
 
