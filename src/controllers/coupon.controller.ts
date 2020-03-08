6e8451965b19f357ef0b623465a0880e9d1b40c0
Tyav <tyav2greenz@gmail.com>
2020-03-08 15:22:57 +0100

    add controller to create coupon

--- src/controllers/coupon.controller.ts
+++ src/controllers/coupon.controller.ts
@@ -1,13 +1,12 @@
-import Coupon, {  } from '../models/coupon.model';
+import Coupon, { ICouponNoExtend } from '../models/coupon.model';
 import sendResponse from '../helpers/response';
 import httpStatus from 'http-status';
 
 
 
 //Get coupon by code
-export const GetCoupon = async (code: String) => {
+export const getCoupon = async (code: String) => {
   try {
-    console.log('sdfsdff')
     const coupon = await Coupon.findOne({ code });
 
     if (!coupon) {
@@ -25,3 +24,41 @@ export const GetCoupon = async (code: String) => {
     throw new Error(error);
   }
 };
+
+export const createCoupon = async ( body: ICouponNoExtend ) => {
+  try {
+    // @ts-ignore
+    const { user, id } = body
+    delete body.user;
+    delete body.id; 
+    body.createdBy = id
+    const couponExist = await Coupon.findOne({ code: body.code })
+    if (couponExist) {
+      return sendResponse(
+        httpStatus.BAD_REQUEST,
+        'Coupon Code already exist',
+        {},
+        null,
+        '',
+      );
+    }
+    const coupon = new Coupon(body);
+    await coupon.save()
+    return sendResponse(
+      httpStatus.CREATED,
+      'Coupon created successfully',
+      coupon,
+      null,
+      '',
+    );
+
+  } catch (error) {
+    return sendResponse(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      error.message,
+      {},
+      null,
+      '',
+    );
+}
+}
