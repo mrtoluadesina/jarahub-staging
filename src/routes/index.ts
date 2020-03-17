1865f25c0b19696556e825a4a0765561070a5431
Tolu Adesina <tadesina90@gmail.com>
2020-03-17 10:52:59 +0100

    add a post url for seeding

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import sampleController from '../controllers/sample';
+import seed from '../db/seed';
 
 const router = Router();
 
@@ -7,6 +8,12 @@ router.get('/', function(_req, res, _next) {
   const message = sampleController();
 
   res.status(200).json({ message });
+});
+
+router.post('/', function(_req, res, _next) {
+  const seedMessage = seed();
+
+  res.status(200).json({ seedMessage });
 });
 
 export default router;
