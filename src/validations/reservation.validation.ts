b5cf74540402e52edf39d49d86670db6559fee3a
Tyav <tyav2greenz@gmail.com>
2020-05-13 22:26:25 +0100

    refactor validation for reservation

--- src/validations/reservation.validation.ts
+++ src/validations/reservation.validation.ts
@@ -2,16 +2,21 @@ import { Joi } from 'celebrate';
 
 export default {
   create: {
-    productIds: Joi.array().items({
-      quantity: Joi.number().required(),
-      productId: Joi.string().pattern(/^[a-fA-F0-9]{24}$/).required()
-    }),
-    userDetails: Joi.object({
-      email: Joi.string().email().required(),
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      phone: Joi.string().required(),
-      address: Joi.string().required()
-    }).required()
+    body: {
+      user: Joi.object(),
+      id: Joi.string().pattern(/^[a-fA-F0-9]{24}$/).required(),
+      productIds: Joi.array().items({
+        quantity: Joi.number().required(),
+        productId: Joi.string().pattern(/^[a-fA-F0-9]{24}$/).required()
+      }),
+      userDetails: Joi.object({
+        email: Joi.string().email().required(),
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        phone: Joi.string().required(),
+        address: Joi.string().required()
+      }).required(),
+      chargedAmount: Joi.number().required(),
+    }
   }
 }
