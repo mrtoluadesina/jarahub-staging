93b622daa0ba86d8f4cd17accaad99e7f2d35106
whitehox <oketegah@gmail.com>
2020-05-16 12:26:21 +0100

    create v2 mail  sender

---
+++ src/helpers/sendMailV2.ts
@@ -0,0 +1,12 @@
+import sgMail from '@sendgrid/mail';
+import { MailDataRequired } from '@sendgrid/helpers/classes/mail';
+
+export default async function sendMailV2(msg: MailDataRequired) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+  sgMail.send({
+    ...msg,
+    mailSettings: {
+      sandboxMode: { enable: process.env.NODE_ENV === 'test' ? true : false },
+    },
+  });
+}
