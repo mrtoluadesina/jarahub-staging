a5dc37e41d40f4dc8a68ea08048b1ae036cf07f5
whitehox <oketegah@gmail.com>
2020-07-24 09:35:30 +0100

    fix review graphql layer

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,10 +1,22 @@
 import Review, { Review as IReview } from '../models/reviews.model';
 import sendResponse from '../helpers/response';
+import Product from '../models/product.model';
 
 export async function create(body: IReview, productId: string) {
   try {
-    const review = new Review({ productId, ...body });
+    const product = await Product.findById(productId);
+    const review = new Review({ ...body, productId });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    product.reviews!.push(review);
+
+    await product.save();
+
     const payload = await review.save();
+
     return sendResponse(200, 'Review Submitted', payload, null, '');
   } catch (error) {
     throw new Error(error.message);
